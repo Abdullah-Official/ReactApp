2b80525c6a135cf8b0dec7209136650642d68181
Abdullah-Official <rugusakii@gmail.com>
2020-10-13 11:27:04 -0700

    Updates

---
+++ precache-manifest.835fc52cd7dc75d4d548ef7dc3820e34.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86111f7a53354ef1414a11228d307346",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "5b31265ff3b117d6a64b",
+    "url": "/ReactApp/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "8f39bf011d795df6581d",
+    "url": "/ReactApp/static/css/main.0cad6359.chunk.css"
+  },
+  {
+    "revision": "5b31265ff3b117d6a64b",
+    "url": "/ReactApp/static/js/2.d5607046.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/ReactApp/static/js/2.d5607046.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f39bf011d795df6581d",
+    "url": "/ReactApp/static/js/main.686adfba.chunk.js"
+  },
+  {
+    "revision": "5645184ac82e5ff09f0c",
+    "url": "/ReactApp/static/js/runtime-main.a92796e7.js"
+  },
+  {
+    "revision": "95c3495f1f04c094bdfb764f33715fab",
+    "url": "/ReactApp/static/media/about.95c3495f.png"
+  },
+  {
+    "revision": "11edf1bd43740da7a59ec103774e39eb",
+    "url": "/ReactApp/static/media/graphic.11edf1bd.jpg"
+  },
+  {
+    "revision": "99df02b01eaa6e55a2eb1f432920d4c5",
+    "url": "/ReactApp/static/media/home.99df02b0.png"
+  },
+  {
+    "revision": "b44b3741097a8dea27f78ecaea732de6",
+    "url": "/ReactApp/static/media/software.b44b3741.jpg"
+  }
+]);
